34c3de1b2c6c5e7a51a924e78c23a7ab6cd3bc00
ElMataBots <mariojosemonrroyduran@gmail.com>
2024-05-28 08:33:30 -0500

    first commit

---
+++ conexion.php
@@ -0,0 +1,25 @@
+<?php 
+
+// Creamos las variables de conexión 
+
+$servername = "localhost"; 
+
+$username = "root"; 
+
+$password = ""; 
+
+$database = "jardineria"; 
+
+// Creamos la conexion con MySQL 
+
+$con = new mysqli($servername, $username, $password, $database); 
+
+// Revisamos la conexión 
+
+if ($con->connect_error) { 
+
+die("Conexión fallida: " . $con->connect_error); 
+
+} 
+
+?> 
